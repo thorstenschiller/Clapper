7caba7245f98ea36da967227f38948e49950d4d6
thorstenschiller <thorstenschiller@googlemail.com>
2021-03-12 17:45:40 +0100

    new init

---
+++ includes/class-clapper-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       thorstenschiller.com
+ * @since      1.0.0
+ *
+ * @package    Clapper
+ * @subpackage Clapper/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Clapper
+ * @subpackage Clapper/includes
+ * @author     Thorsten <info@thorstenschiller.com>
+ */
+class Clapper_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-clapper-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       thorstenschiller.com
+ * @since      1.0.0
+ *
+ * @package    Clapper
+ * @subpackage Clapper/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Clapper
+ * @subpackage Clapper/includes
+ * @author     Thorsten <info@thorstenschiller.com>
+ */
+class Clapper_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}

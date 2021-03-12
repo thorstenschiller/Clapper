3374bb8070c674c38d406c7db62053d974bdd21b
thorstenschiller <thorstenschiller@googlemail.com>
2021-03-12 18:23:30 +0100

    add field to database
    class-clapper-activator in includes

--- includes/class-clapper-activator.php
+++ includes/class-clapper-activator.php
@@ -34,3 +34,20 @@ class Clapper_Activator {
 	}
 
 }
+global $wpdb;
+$plugin_name_db_version = '1.0';
+$table_name = $wpdb->prefix . "clapper_date"; 
+$charset_collate = $wpdb->get_charset_collate();
+
+$sql = "CREATE TABLE $table_name (
+		  id mediumint(9) NOT NULL AUTO_INCREMENT,
+		  created timestamp NOT NULL default CURRENT_TIMESTAMP,
+		  name tinytext NULL,
+		  custom_field varchar(255) DEFAULT '' NOT NULL,
+		  email varchar(255) DEFAULT '' NOT NULL,
+		  UNIQUE KEY id (id)
+		) $charset_collate;";
+
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+dbDelta( $sql );
+add_option( 'plugin_name_db_version', $plugin_name_db_version );
